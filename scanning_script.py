d193ad7b38f8e922ec9746b3e154e2f0d4f7ece0
chiseng <thehedge96@hotmail.com>
2018-12-22 12:18:10 +0800

    fixed bugs

--- scanning_script.py
+++ scanning_script.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
+
+
 #Run file as following: scanning_script.py <target host IP> y (if you want dirs to be created to store stuff)
 import subprocess
 import nmap
 import sys
+try:
+    target=sys.argv[1]
+except IndexError:
+    print('Example: ./scanning_script.py 1.3.3.7 <y>')
+    sys.exit(-1) #exit when error
 
+    
 def processor(process): #huge creds to phi10s 
     output=""
     while process.poll() is None:
@@ -15,20 +23,24 @@ def processor(process): #huge creds to phi10s
     sys.stdout.write(readline)
     sys.stdout.flush()
     return output
-if(sys.argv[3]=='y'):
-    try:
-        subprocess.call(['mkdir',sys.argv[1]])
-        subprocess.call(['cd',sys.argv[1]])
-        subprocess.call(['mkdir','scripts'])
-        subprocess.call(['mkdir','exploits'])
-        subprocess.call(['mkdir','scans'])
-	print('')
-    except Exception, e:
-        print('Directory not created')
+try:
+    if(sys.argv[2]=='y'):
+        try:
+            subprocess.call(['mkdir',sys.argv[1]])
+            subprocess.call(['cd',sys.argv[1]])
+            subprocess.call(['mkdir','scripts'])
+            subprocess.call(['mkdir','exploits'])
+            subprocess.call(['mkdir','scans'])
+	    print('')
+        except OSError, e:
+            print('Directory not created')
+            print('')
+except IndexError:
+    pass
 print('===========================================') #i was too lazy to make one for nmap :<
 print('Nmap service running')
 nm=nmap.PortScanner()
-nm.scan(sys.argv[1], sys.argv[2]) #scan ip, ports
+nm.scan(sys.argv[1], '10-8080') #scan ip, ports
 host=nm.all_hosts()[0]
 port80=[]
 for protocol in nm[host].all_protocols():
@@ -45,7 +57,6 @@ for protocol in nm[host].all_protocols():
             print ('port : %s\tstate : %s' % (port, nm[host][protocol][port]['state']))
 print('===========================================')
 print('')
-print(host)
 if 80 in port80:
     print('Get a drink or something')
     print('\n  ___'
